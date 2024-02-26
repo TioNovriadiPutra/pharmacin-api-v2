eaba08c8c073a4ab6d41751961f1326062382de4
ambrololol <triadiwicaksana@gmail.com>
2024-02-26 22:53:54 +0700

    new: migration selling transaction

---
+++ database/migrations/1708954873051_create_selling_shopping_carts_table.ts
@@ -0,0 +1,23 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'selling_shopping_carts'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+
+      table.integer('selling_transaction_id').unsigned().references('id').inTable('selling_transactions').notNullable().onDelete('cascade')
+      table.integer('item_name')
+      table.integer('item_price')
+      table.integer('item_qty')
+
+      table.timestamp('created_at').notNullable().defaultTo(this.now())
+      table.timestamp('updated_at').notNullable().defaultTo(this.now())
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
